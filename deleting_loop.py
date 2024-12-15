b0727a539100c8fd83a5462186ece9f04dacefd0
Simon Vargas <sjvargas01@gmail.com>
2024-12-15 17:02:46 -0500

    updates

---
+++ deleting_loop.py
@@ -0,0 +1,11 @@
+greetings = ['hello', 'hi', 'hey', 'hola', 'hi', 'hello', 'hi']
+newGreetings = []
+newGreetings2 = [word for word in greetings if word == 'hi']
+
+for word in greetings:
+    if word == 'hi':
+        newGreetings.append(word)
+
+greetings = newGreetings
+print(greetings)
+print("newGreetings2: ", newGreetings2)
