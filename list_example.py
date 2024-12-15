6d94601bd9a8a1e35fdad84bdbd9f4bff01861ac
Simon Vargas <sjvargas01@gmail.com>
2024-12-14 21:41:06 -0500

    list example

---
+++ list_example.py
@@ -0,0 +1,22 @@
+clothes = ['shirts', 'pants', 'shorts', 'jackets', 'socks']
+new_clothes = []
+
+for item in clothes:
+    if item == 'shirts':
+        new_clothes.append('t-shirts')
+    elif item == 'pants':
+        new_clothes.append('jeans')
+    elif item == 'shorts':
+        new_clothes.append('swim trunks')
+    elif item == 'jackets':
+        new_clothes.append('hoodies')
+    elif item == 'socks':
+        new_clothes.append('slippers')
+    else:
+     new_clothes.append(item)
+
+print(new_clothes)
+
+clothes.extend(new_clothes)
+
+print(clothes)
