a4dc751094b7573e07ac451f0044c7ccca3f2441
Simon Vargas <sjvargas01@gmail.com>
2025-01-17 20:45:49 -0500

    list_example added function for area of square

--- list_example.py
+++ list_example.py
@@ -1,22 +1,26 @@
-clothes = ['shirts', 'pants', 'shorts', 'jackets', 'socks']
-new_clothes = []
+# clothes = ['shirts', 'pants', 'shorts', 'jackets', 'socks']
+# new_clothes = []
 
-for item in clothes:
-    if item == 'shirts':
-        new_clothes.append('t-shirts')
-    elif item == 'pants':
-        new_clothes.append('jeans')
-    elif item == 'shorts':
-        new_clothes.append('swim trunks')
-    elif item == 'jackets':
-        new_clothes.append('hoodies')
-    elif item == 'socks':
-        new_clothes.append('slippers')
-    else:
-     new_clothes.append(item)
+# for item in clothes:
+#     if item == 'shirts':
+#         new_clothes.append('t-shirts')
+#     elif item == 'pants':
+#         new_clothes.append('jeans')
+#     elif item == 'shorts':
+#         new_clothes.append('swim trunks')
+#     elif item == 'jackets':
+#         new_clothes.append('hoodies')
+#     elif item == 'socks':
+#         new_clothes.append('slippers')
+#     else:
+#      new_clothes.append(item)
 
-print(new_clothes)
+# print(new_clothes)
 
-clothes.extend(new_clothes)
+# clothes.extend(new_clothes)
 
-print(clothes)
+# print(clothes)
+def square_area(side):
+    return side * side
+
+print(square_area(7))
